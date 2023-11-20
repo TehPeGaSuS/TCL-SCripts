add022afb7e281e36cb5555305f86a83c41fb466
PeGaSuS <droider.pc@gmail.com>
2023-11-20 23:46:44 +0100

    Update idlerpg.tcl

--- Eggdrop/idlerpg.tcl
+++ Eggdrop/idlerpg.tcl
@@ -27,7 +27,8 @@ namespace eval idlerpg {
     proc idleJoin {nick uhost hand chan} {
         if {$chan eq "$::idlerpg::Chan"} {
             if {$nick eq "$::idlerpg::Bot"} {
-                putlog "Sending LOGIN command because bot $::idlerpg::Bot just entered the channel"
+                putlog "Sending LOGIN command because $::idlerpg::Bot bot just entered the channel"
+                putlog "This is just in case wee need to relogin"
                 putserv "PRIVMSG $nick :LOGIN $::idlerpg::Character $::idlerpg::Password"
                 return 0
             }
